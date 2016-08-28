7efe91de9e6c9e3793632947bec9975f9b55f4df
Aaron Vegh <aaron@innoveghtive.com>
2016-08-28 14:25:31 -0400

    Initial Commit

---
+++ Fangirls/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Fangirls
+//
+//  Created by Aaron Vegh on 2016-08-28.
+//  Copyright © 2016 Aaron Vegh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Fangirls/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Fangirls
+//
+//  Created by Aaron Vegh on 2016-08-28.
+//  Copyright © 2016 Aaron Vegh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
